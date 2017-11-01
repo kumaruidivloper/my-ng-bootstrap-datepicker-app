7350022f1653cf8b361d0006117eebba3b13c69e
kumaravel shanmugam <kumaruidivloper@gmail.com>
2017-11-01 13:22:29 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { JsonpModule } from '@angular/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbdDatepickerPopup } from './datepicker-popup';
 
-import { AppComponent } from './app.component';
+@Component({
+  selector: 'my-app',
+  template: `
+    <div class="container-fluid">
+    
+    <hr>
+    <p>
+      This is a demo plnkr forked from the <strong>ng-bootstrap</strong> project: Angular powered Bootstrap.
+      Visit <a href="https://ng-bootstrap.github.io/" target="_blank">https://ng-bootstrap.github.io</a> for more widgets and demos.
+    </p>
+    <hr>
+
+    <ngbd-datepicker-popup></ngbd-datepicker-popup>
+  </div>
+  `
+})
+export class App {
+}   
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, JsonpModule, NgbModule.forRoot()], 
+  declarations: [App, NgbdDatepickerPopup],
+  bootstrap: [App]
+}) 
+export class AppModule {}
